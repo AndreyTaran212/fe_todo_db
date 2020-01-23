c1ed31a77b7d870f32eb9645d64c7d054d1f8290
AndreyT <tarangad313@gmail.com>
2020-01-23 20:39:18 +0200

    second circle of hell

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -1,42 +1,47 @@
 'use strict';
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isAlpha: true,
-      }
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isAlpha: true,
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isEmail: true,
-      }
-    },
-    login: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: [6,16],
-      }
-    },
-    passwordHash: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+	const User = sequelize.define('User', {
+		firstName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isAlpha: true,
+			}
+		},
+		lastName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isAlpha: true,
+			}
+		},
+		email: {
+			type: DataTypes.STRING,
+			unique: true,
+			validate: {
+				isEmail: true,
+			}
+		},
+		login: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				len: [6, 16],
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			field: 'passwordHash',
+			set (val) {
+				this.setDataValue('password', bcrypt.hash(val, 10));
+			}
+		}
+	}, {});
+	User.associate = function (models) {
+		// associations can be defined here
+	};
+	return User;
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,45 +1,84 @@
-const { User } = require('./db/models');
-const bcrypt = require('bcrypt');
+import { User, sequelize }  from './db/models';
+import { Model, DataTypes } from 'sequelize';
+import moment               from 'moment';
 
-const hashPassword = async password => {
-	try {
-		return bcrypt.hash(password, 10);
-	} catch (e) {
-		return e;
-	}
-};
+/*const bcrypt = require('bcrypt');*/
 
-const createUser = async data => {
-	try {
-		data.passwordHash = await hashPassword(data.password);
-		const createUser = await User.create(data);
-		if (createUser) {
-			return createUser.get();
-		}
-		new Error();
-	} catch (e) {
-		throw e;
-	}
-};
+/*
+ const hashPassword = async password => {
+ try {
+ return bcrypt.hash(password, 10);
+ } catch (e) {
+ return e;
+ }
+ };
 
-/*createUser({
-						 firstName: 'Name',
-						 lastName: 'Surname',
-						 email: '1111@gmail.com',
-						 login: 'VasyaVasya',
-						 password: '12341234'
-					 })
-	.then(console.log)
-	.catch(console.error);*/
+ const createUser = async data => {
+ try {
+ data.passwordHash = await hashPassword(data.password);
+ const createUser = await User.create(data);
+ if (createUser) {
+ return createUser.get();
+ }
+ new Error();
+ } catch (e) {
+ throw e;
+ }
+ };
 
+ /!*createUser({
+ firstName: 'Name',
+ lastName: 'Surname',
+ email: '1111@gmail.com',
+ login: 'VasyaVasya',
+ password: '12341234'
+ })
+ .then(console.log)
+ .catch(console.error);*!/
 
-const getUserById = async data =>{
-try {
-	const searchUser = await User.findByPk(data);
-	return searchUser.get()
-}catch (e) {
-	throw e;
+ const getUserById = async id => {
+ try {
+ return (await User.findByPk(id)).get();
+ } catch (e) {
+ throw e;
+ }
+ };
+
+ getUserById(101).then(console.log);*/
+
+class Task extends Model {
+
 }
-};
 
-getUserById(101).then(console.log);
+Task.init({
+						value: {
+							type: DataTypes.STRING,
+							allowNull: false,
+							validate: {
+								notEmpty: true,
+
+							}
+						},
+						deadLine: {
+							type: DataTypes.DATE,
+							allowNull: false,
+							validate: {
+								isDate: true,
+								isAfter: new Date(),
+							}
+						},
+						isDone: {
+							type: DataTypes.BOOLEAN,
+							defaultValue: false,
+							allowNull: false,
+						}
+					}, {
+						sequelize,
+						timestamps: true,
+					});
+
+Task.belongsTo(User);
+User.hasMany(Task);
+
+Task.sync();
+
